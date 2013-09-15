2473c0daf4b9288b796db3dc17b25f4ed7f7a667
Dmitry Neverov <dmitry.neverov@gmail.com>
2013-09-15 23:55:53 +0400

    load init file when server is loaded

--- cljrepl-server/src/main/resources/init.clj
+++ cljrepl-server/src/main/resources/init.clj
@@ -1,10 +1,13 @@
 (in-ns 'user)
-
 (require '(tc))
-
-(let [init (java.io.File. (.getConfigDir tc/server-paths) "init.clj")]
-  (when (.exists init)
-    (load-file (.getAbsolutePath init))))
 
 (def nrepl (start-server))
 (println (str "nrepl started at port " (:port nrepl)))
+
+(.addListener
+ tc/events
+ (proxy [jetbrains.buildServer.serverSide.BuildServerAdapter] []
+     (serverStartup []
+                    (let [init (java.io.File. (.getConfigDir tc/server-paths) "init.clj")]
+                      (when (.exists init)
+                        (load-file (.getAbsolutePath init)))))))
